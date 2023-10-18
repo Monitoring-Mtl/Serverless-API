07648a12c491e7f9760b50888b435b1e5170db24
pabdelli <60405217+nerdz88@users.noreply.github.com>
2023-10-18 10:28:51 -0400

    remove aws sam build folder

--- .aws-sam/build/ExpressFunction/.prettierrc.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    arrowParens: 'always',
-    tabWidth: 4,
-    printWidth: 120,
-};
--- .aws-sam/build/ExpressFunction/src/index.ts
+++
@@ -1,5 +0,0 @@
-import serverless from 'serverless-http';
-import app from './app';
-
-//Serverless wrapper for express app
-export const handler = serverless(app);
